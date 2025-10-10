1d95211effc6b6ea933b38b4040e86493bbdbd01
Ankitha-hassan <ankitahassan@gmail.com>
2025-10-10 11:18:59 +0530

    added 3 entities

---
+++ Leadership.Domain/Models/LeaderBoard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leadership.Domain.Models
+{
+    public class LeaderBoard
+    {
+        public int LeaderboardId { get; set; }
+
+        public int QuizId { get; set; }
+        public Quiz Quiz { get; set; }
+
+        public int ParticipantId { get; set; }
+        public Participant Participant { get; set; }
+
+        public int Score { get; set; }
+        public int Rank { get; set; }
+        //public string ParticipantName { get; set; }
+    }
+}
---
+++ Leadership.Domain/Models/Participant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leadership.Domain.Models
+{
+    public class Participant
+    {
+        public int ParticipantId { get; set; }
+        public int UserId { get; set; } 
+
+        public int QuizId { get; set; }
+        public Quiz Quiz { get; set; }
+
+        public int Score { get; set; }
+
+        public ICollection<Response> Responses { get; set; }
+    }
+}
